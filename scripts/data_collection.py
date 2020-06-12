654baae69b72f1efeb3bd37f881478dc462c904e
Rubén Rodríguez <rubenrod.018@gmail.com>
2020-06-12 13:28:14 +0200

    chore: WIP

--- scripts/data_collection.py
+++ scripts/data_collection.py
@@ -138,7 +138,7 @@ def _get_hard_disk_data() -> list:
 
 
 def _get_network_data() -> list:
-    data = subprocess.run(['vnstat --oneline'])
+    data = subprocess.run(['vnstat', '--oneline'])
     data = data.split(';')
 
     private_ip_command = "ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
